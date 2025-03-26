32afdba0ff3192cb221217d454314e459b186b48
zr123 <25320035+zr123@users.noreply.github.com>
2025-03-26 01:18:27 +0100

    Reworked Private Security

--- src/server/cards/pathfinders/PrivateSecurity.ts
+++ src/server/cards/pathfinders/PrivateSecurity.ts
@@ -4,7 +4,6 @@ import {CardType} from '../../../common/cards/CardType';
 import {CardName} from '../../../common/cards/CardName';
 import {CardRenderer} from '../render/CardRenderer';
 import {Tag} from '../../../common/cards/Tag';
-import {Size} from '../../../common/cards/render/Size';
 
 export class PrivateSecurity extends Card implements IProjectCard {
   constructor() {
@@ -14,11 +13,16 @@ export class PrivateSecurity extends Card implements IProjectCard {
       cost: 8,
       tags: [Tag.EARTH],
 
+      action: {
+        spend: {megacredits: 3},
+        underworld: {corruption: 1},
+      },
+
       metadata: {
         cardNumber: 'Pf25',
         renderData: CardRenderer.builder((b) => {
-          b.text('Opponents may not remove your basic resource production', Size.SMALL, true).br;
-          b.production((pb) => pb.wild(1, {cancelled: true}));
+          b.action('Spend 3M€ to gain 1 curruption.',
+            (ab) => ab.megacredits(3).startAction.corruption(1)).br;
         }),
       },
     });
