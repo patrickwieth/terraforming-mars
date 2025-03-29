a15876f920ed18288ad0301ec3e8c9ff12f9fde3
zr123 <25320035+zr123@users.noreply.github.com>
2025-03-29 16:11:28 +0100

    reworked sabotage

--- src/server/cards/base/Sabotage.ts
+++ src/server/cards/base/Sabotage.ts
@@ -1,83 +1,29 @@
 import {IProjectCard} from '../IProjectCard';
 import {Card} from '../Card';
 import {CardType} from '../../../common/cards/CardType';
-import {IPlayer} from '../../IPlayer';
-import {OrOptions} from '../../inputs/OrOptions';
-import {Resource} from '../../../common/Resource';
 import {CardName} from '../../../common/cards/CardName';
-import {SelectOption} from '../../inputs/SelectOption';
 import {CardRenderer} from '../render/CardRenderer';
-import {Size} from '../../../common/cards/render/Size';
-import {all, digit} from '../Options';
-import {message} from '../../logs/MessageBuilder';
 
 export class Sabotage extends Card implements IProjectCard {
   constructor() {
     super({
       type: CardType.EVENT,
       name: CardName.SABOTAGE,
+      victoryPoints: -2,
       cost: 1,
+
+      behavior: {
+        underworld: {corruption: 2},
+      },
 
       metadata: {
         cardNumber: '121',
         renderData: CardRenderer.builder((b) => {
-          b.minus().titanium(3, {all, digit}).nbsp.or(Size.SMALL).nbsp;
-          b.minus().steel(4, {all, digit}).br.or(Size.SMALL).nbsp;
-          b.minus().megacredits(7, {all});
+          b.corruption(2);
         }),
-        description: 'Remove up to 3 titanium from any player, or 4 steel, or 7 M€.',
+        description: 'Gain 2 corruption.',
       },
     });
-  }
-
-  private title(amount: number, type: string, target: IPlayer) {
-    return message('Remove ${0} ${1} from ${2}', (b) => b.number(amount).string(type).player(target));
-  }
-
-  public override bespokePlay(player: IPlayer) {
-    if (player.game.isSoloMode()) {
-      player.resolveInsuranceInSoloGame();
-      return undefined;
-    }
-
-    const availableActions = new OrOptions();
-
-    player.getOpponents().forEach((target) => {
-      if (target.titanium > 0 && !target.alloysAreProtected()) {
-        const amountRemoved = Math.min(3, target.titanium);
-        const optionTitle = this.title(amountRemoved, 'titanium', target);
-        availableActions.options.push(new SelectOption(optionTitle).andThen(() => {
-          target.stock.deduct(Resource.TITANIUM, 3, {log: true, from: player});
-          return undefined;
-        }));
-      }
-
-      if (target.steel > 0 && !target.alloysAreProtected()) {
-        const amountRemoved = Math.min(4, target.steel);
-        const optionTitle = this.title(amountRemoved, 'steel', target);
-        availableActions.options.push(new SelectOption(optionTitle).andThen(() => {
-          target.stock.deduct(Resource.STEEL, 4, {log: true, from: player});
-          return undefined;
-        }));
-      }
-
-      if (target.megaCredits > 0) {
-        const amountRemoved = Math.min(7, target.megaCredits);
-        const optionTitle = this.title(amountRemoved, 'M€', target);
-        availableActions.options.push(new SelectOption(optionTitle).andThen(() => {
-          target.stock.deduct(Resource.MEGACREDITS, 7, {log: true, from: player});
-          return undefined;
-        }));
-      }
-    });
-
-    if (availableActions.options.length > 0) {
-      availableActions.options.push(new SelectOption('Do not remove resource').andThen(() => {
-        return undefined;
-      }));
-      return availableActions;
-    }
-    return undefined;
   }
 }
 
